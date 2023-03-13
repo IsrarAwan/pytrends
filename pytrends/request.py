80bb24669df78a5005d0912742d97d430ef4033e
Israr <israr.inspiration@gmail.com>
2023-03-13 10:30:44 +0100

    Update request.py

--- pytrends/request.py
+++ pytrends/request.py
@@ -6,7 +6,7 @@ import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 from requests import status_codes
-from pytrends.pytrends import exceptions
+from pytrends import exceptions
 from urllib.parse import quote
 
 BASE_TRENDS_URL = 'https://trends.google.com/trends'
